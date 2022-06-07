fb97ae2427cd7b069b8329fabc8acb05a6124f29
Alessandro Lira <Alessandro.Lira@uniforja.com.br>
2022-06-07 10:58:50 -0300

    preciso inserir um meio para alocar as questões

--- src/main.java
+++ src/main.java
@@ -6,22 +6,26 @@ public class main {
 	public static void main(String[] args) {
 		
 		Perguntas questao1 = new Perguntas("A terra é plana?","Sim", "Não", "Com certeza não", 1);
+		Perguntas questao2 = new Perguntas("Qual a cor do elefante", "preta", "azul", "cinza", 3);
 		
-		System.out.println(questao1.getPergunta());
-		System.out.println("°°°  " + questao1.getPrimeiraAlternativa());
-		System.out.println("°°°  " + questao1.getSegundaAlternativa());
-		System.out.println("°°°  " + questao1.getTerceiraAlternativa());
-		
-		Scanner entrada = new Scanner(System.in);
-		int palpite = entrada.nextInt();
-		entrada.close();
-		
-		if (palpite == questao1.resposta) {
-			System.out.println("PARABÉNS!!, Você acertou");;
-		}else {
-			System.out.println("infelizmente você errou!");;
+		for(int i = 0; true; i += 1) {
+			System.out.println(questao2.getPergunta());
+			System.out.println("°°°  " + questao2.getPrimeiraAlternativa());
+			System.out.println("°°°  " + questao2.getSegundaAlternativa());
+			System.out.println("°°°  " + questao2.getTerceiraAlternativa());
+			
+			Scanner entrada = new Scanner(System.in);
+			int palpite = entrada.nextInt();
+			entrada.close();
+			
+			if (palpite == questao2.resposta) {
+				System.out.println("PARABÉNS!!, Você acertou");;
+				return;
+			}else {
+				System.out.println("infelizmente você errou!");;
+				break;
+			}
 		}
-		
 	}
 
 }
